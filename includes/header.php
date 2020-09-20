905ba09c7b3772cdf86fc2bee852cedfe3caf903
Hawra Alomani <ha0073@mix.wvu.edu>
2020-09-20 11:10:19 -0400

    changed localhost to 127.0.0.1 (header.php)

--- includes/header.php
+++ includes/header.php
@@ -21,8 +21,8 @@
 
     </head>
     <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Fanatic</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light >
+        <a class="navbar-brand" href="#">AnimalCrossing Guide</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -39,18 +39,18 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Reviews
+                        Items
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Replace Me</a>
+                        <a class="dropdown-item" href="#">Plants</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Replace Me 2</a>
+                        <a class="dropdown-item" href="#">Sea Creatures</a>
                     </div>
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
     </nav>
